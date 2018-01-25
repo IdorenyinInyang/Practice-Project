8cc4f0612465adcb83e836f0c307ae3f5d50e5ad
IdorenyinInyang <iainyang@aggies.ncat.edu>
2018-01-25 13:31:43 -0500

    Implemented JavaFX
    * Changed access rules in Eclipse for JavaFX
    * created GUI from JavaFX package
    * added terminology text file for JavaFX

--- PracticeProject/src/PracticeClass.java
+++ PracticeProject/src/PracticeClass.java
@@ -1,9 +1,34 @@
 
-public class PracticeClass {
+import javafx.application.Application;  
+import javafx.event.ActionEvent; 
+import javafx.event.EventHandler;  
+import javafx.scene.Scene; 
+import javafx.scene.control.Button; 
+import javafx.scene.layout.StackPane; 
+import javafx.stage.Stage;
+
+
+
+
+public class PracticeClass extends Application{ 
+	
+	Button button;
 
 	public static void main(String[] args) {
-		System.out.println("Testing git capabilities in new IDE");
-
+		launch(args);
+	} 
+	
+	public void start(Stage primaryStage) throws Exception { 
+		primaryStage.setTitle("Practice JavaFX Window");	
+		button = new Button(); //
+		button.setText("Click Me!"); // second alternative, you can put title in initial button declaration parameters 
+		
+		StackPane layout = new StackPane(); 
+		layout.getChildren().add(button); 
+		
+		Scene scene = new Scene(layout, 300, 250); //by default, 
+		primaryStage.setScene(scene); 
+		primaryStage.show();
 	}
 
 }

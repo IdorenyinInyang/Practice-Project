9b13ce811628d1c7903282bfb55438e3db82c654
IdorenyinInyang <iainyang@aggies.ncat.edu>
2018-02-20 14:23:35 -0500

    Parameter changes
    Coordinate points on the Canvas are represented in doubles, not integers.

--- PracticeProject/src/Pencil.java
+++ PracticeProject/src/Pencil.java
@@ -3,19 +3,23 @@ import javafx.event.EventHandler;
 
 public class Pencil extends Tools{ 
 	
-	int x, y; //instance variables representing the 
+	double x, y; //instance variables representing the 
 	
 	public Pencil(){  
 		x = 0; 
 		y = 0;
 	} 
 	
-	public Pencil(int x, int y) { 
+	public Pencil(double x, double y) { 
 		x = this.x; 
 		y = this.y; 
+	} 
+	
+	public void toDraw(double x, double y) { 
+		System.out.println(x+" "+y);
 	}
 	
-	public void serializeDraw(int x, int y) { 
+	public void serializeDraw(double x, double y) { 
 		
 	}
 }
--- PracticeProject/src/Tools.java
+++ PracticeProject/src/Tools.java
@@ -1,18 +1,21 @@
 
 public class Tools {
 	
-	int x, y; //instance variables representing x and y coordinates
-	
+	double x, y; //instance variables representing x and y coordinates
+	static boolean pen, erasure; // boolean varaibles to represent 
+
 	public Tools() { 
 		x = 0; 
-		y = 0; //instantiate variables
+		y = 0; //instantiate variables 
+		pen = true; 
+		erasure = false;
 	}
-	public Tools(int x, int y) { 
+	public Tools(double x, double y) { 
 		x = this.x; 
 		y = this.y;
 	} 
 	
-	public void serializeDraw(int x, int y) { 
+	public void serializeDraw(double x, double y) { 
 		
 	} 
 }

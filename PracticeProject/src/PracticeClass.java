ee3a9c367b69d62cfcf1a21e30894ffa80652902
IdorenyinInyang <iainyang@aggies.ncat.edu>
2018-02-19 22:46:49 -0500

    Implemented Drawable functionality
    * Removed testing javafx objects
    * Implemented MouseEvent Handlers to track mouse movement
    * set default graphic stroke to blue

--- PracticeProject/src/PracticeClass.java
+++ PracticeProject/src/PracticeClass.java
@@ -6,7 +6,9 @@ import javafx.scene.canvas.*;
 import javafx.scene.Group;
 import javafx.scene.Scene;  
 import javafx.scene.paint.*;
-import javafx.scene.control.Button; 
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.StackPane; 
 import javafx.stage.Stage;
 
@@ -15,25 +17,72 @@ import javafx.stage.Stage;
 
 public class PracticeClass extends Application{ 
 	
-	Button button; 
-	Canvas canvas;
-
 	public static void main(String[] args) {
-		launch(args);
-	} 
+		launch(PracticeClass.class);
+	}  
+	
 	
 	public void start(Stage primaryStage) throws Exception { 
 		primaryStage.setTitle("Practice JavaFX Window");	 
-		//GraphicsContext gc = canvas.getGraphicsContext2D(); 
-		Group root = new Group();
-		button = new Button(); 
-		button.setText("Click Me!"); // second alternative, you can put title in initial button declaration parameters 
-		
-		StackPane layout = new StackPane(); 
-		layout.getChildren().add(button); 
-		
-		Scene scene = new Scene(layout, 300, 250); //by default, 
-		primaryStage.setScene(scene); 
+		StackPane stackP = new StackPane();
+		Canvas canvas = new Canvas(1500, 800);  
+		final GraphicsContext gc = canvas.getGraphicsContext2D();  
+		initDraw(gc);
+
+        canvas.addEventHandler(MouseEvent.MOUSE_PRESSED, 
+                new EventHandler<MouseEvent>(){
+
+            @Override
+            public void handle(MouseEvent event) {
+                gc.beginPath();
+                gc.moveTo(event.getX(), event.getY());
+                gc.stroke();
+            }
+        });
+
+        canvas.addEventHandler(MouseEvent.MOUSE_DRAGGED, 
+                new EventHandler<MouseEvent>(){
+
+            @Override
+            public void handle(MouseEvent event) {
+                gc.lineTo(event.getX(), event.getY());
+                gc.stroke();
+            }
+        });
+
+        canvas.addEventHandler(MouseEvent.MOUSE_RELEASED, 
+                new EventHandler<MouseEvent>(){
+
+            @Override
+            public void handle(MouseEvent event) {
+
+            }
+        });
+        
+        stackP.getChildren().add(canvas);
+		primaryStage.setScene(new Scene(stackP, 1500, 800)); 
 		primaryStage.show(); 
+	} 
+	
+	public void initDraw(GraphicsContext gc) { 
+		double canvasWidth = gc.getCanvas().getWidth();
+        double canvasHeight = gc.getCanvas().getHeight();
+
+        gc.setFill(Color.LIGHTGRAY);
+        gc.setStroke(Color.BLACK);
+        gc.setLineWidth(5);
+
+        gc.fill();
+        gc.strokeRect(
+                0,              //x of the upper left corner
+                0,              //y of the upper left corner
+                canvasWidth,    //width of the rectangle
+                canvasHeight);  //height of the rectangle
+
+        gc.setFill(Color.RED);
+        gc.setStroke(Color.BLUE);
+        gc.setLineWidth(1);
+
+		
 	}
 }

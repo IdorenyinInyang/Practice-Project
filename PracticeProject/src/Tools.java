334799bbf29a740b9994dd8bd3edabeb834632f6
IdorenyinInyang <iainyang@aggies.ncat.edu>
2018-02-19 20:50:20 -0500

    Implementing Tool class
    The tool class is the parent class that will host all the tools utilized within the class.

---
+++ PracticeProject/src/Tools.java
@@ -0,0 +1,18 @@
+
+public class Tools {
+	
+	int x, y; //instance variables representing x and y coordinates
+	
+	public Tools() { 
+		x = 0; 
+		y = 0; //instantiate variables
+	}
+	public Tools(int x, int y) { 
+		x = this.x; 
+		y = this.y;
+	} 
+	
+	public void draw(int x, int y) { 
+		
+	} 
+}

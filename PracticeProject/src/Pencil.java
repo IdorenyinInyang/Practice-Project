2a80b6a5bd86d878fdd7a9642c02cd206f82fd07
IdorenyinInyang <iainyang@aggies.ncat.edu>
2018-02-19 20:58:23 -0500

    Constructor and methods
    * Added default and overloaded constructor
    * Added draw method for when the tool is applied onto canvas

--- PracticeProject/src/Pencil.java
+++ PracticeProject/src/Pencil.java
@@ -1,4 +1,19 @@
 
-public class Pencil {
-
+public class Pencil extends Tools{ 
+	
+	int x, y; //instance variables representing the 
+	
+	public Pencil(){  
+		x = 0; 
+		y = 0;
+	} 
+	
+	public Pencil(int x, int y) { 
+		x = this.x; 
+		y = this.y; 
+	}
+	
+	public void draw(int x, int y) { 
+		
+	}
 }

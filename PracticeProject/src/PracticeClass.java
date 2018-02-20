d5c5deb2c38731fc3d651861a5a318d0ee470f2f
IdorenyinInyang <iainyang@aggies.ncat.edu>
2018-02-19 20:49:32 -0500

    Adding canvas and button objects
    Creating canvas and button objects

--- PracticeProject/src/PracticeClass.java
+++ PracticeProject/src/PracticeClass.java
@@ -2,7 +2,10 @@
 import javafx.application.Application;  
 import javafx.event.ActionEvent; 
 import javafx.event.EventHandler;  
-import javafx.scene.Scene; 
+import javafx.scene.canvas.*;
+import javafx.scene.Group;
+import javafx.scene.Scene;  
+import javafx.scene.paint.*;
 import javafx.scene.control.Button; 
 import javafx.scene.layout.StackPane; 
 import javafx.stage.Stage;
@@ -12,15 +15,18 @@ import javafx.stage.Stage;
 
 public class PracticeClass extends Application{ 
 	
-	Button button;
+	Button button; 
+	Canvas canvas;
 
 	public static void main(String[] args) {
 		launch(args);
 	} 
 	
 	public void start(Stage primaryStage) throws Exception { 
-		primaryStage.setTitle("Practice JavaFX Window");	
-		button = new Button(); //
+		primaryStage.setTitle("Practice JavaFX Window");	 
+		//GraphicsContext gc = canvas.getGraphicsContext2D(); 
+		Group root = new Group();
+		button = new Button(); 
 		button.setText("Click Me!"); // second alternative, you can put title in initial button declaration parameters 
 		
 		StackPane layout = new StackPane(); 
@@ -28,7 +34,6 @@ public class PracticeClass extends Application{
 		
 		Scene scene = new Scene(layout, 300, 250); //by default, 
 		primaryStage.setScene(scene); 
-		primaryStage.show();
+		primaryStage.show(); 
 	}
-
 }
